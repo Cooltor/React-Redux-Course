075f53bbb9b56dd7486142989dc07faa2587d9fc
Cooltor <romaindbus86@gmail.com>
2023-03-22 15:56:47 +0100

    done with map method

--- 02-react-base-picker/src/components/MenuList/MenuList.jsx
+++ 02-react-base-picker/src/components/MenuList/MenuList.jsx
@@ -1,29 +1,18 @@
 import { MenuListItem } from "../MenuListItem/MenuListItem";
 import s from "./style.module.css";
+import { DIFFICULTIES } from "./constantes";
 
 export function MenuList({ difficulty, onItemClick }) {
   return (
     <div className={s.container}>
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="low"
-        isSelected={difficulty === "low"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="medium"
-        isSelected={difficulty === "medium"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="high"
-        isSelected={difficulty === "high"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="insane"
-        isSelected={difficulty === "insane"}
-      />
+      {DIFFICULTIES.map((item) => (
+        <MenuListItem
+          key={item}
+          difficulty={item}
+          isSelected={item === difficulty}
+          onClick={onItemClick}
+        />
+      ))}
     </div>
   );
 }
